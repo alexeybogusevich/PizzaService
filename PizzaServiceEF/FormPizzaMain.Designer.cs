1455810c5a3897c15a83c4c2c001b967068e2447
alexeybogusevich <alexey.bogusevich@gmail.com>
2019-04-03 18:23:13 +0300

    MainFormJoined

---
+++ PizzaServiceEF/FormPizzaMain.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace PizzaServiceEF
+{
+    partial class FormPizzaService
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPizzaService));
+            this.buttonOrder = new System.Windows.Forms.Button();
+            this.buttonRestaurants = new System.Windows.Forms.Button();
+            this.buttonComment = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonOrder
+            // 
+            this.buttonOrder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.buttonOrder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonOrder.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOrder.ForeColor = System.Drawing.Color.White;
+            this.buttonOrder.Location = new System.Drawing.Point(-1, 155);
+            this.buttonOrder.Name = "buttonOrder";
+            this.buttonOrder.Size = new System.Drawing.Size(263, 56);
+            this.buttonOrder.TabIndex = 0;
+            this.buttonOrder.Text = "Зробити замовлення";
+            this.buttonOrder.UseVisualStyleBackColor = false;
+            this.buttonOrder.Click += new System.EventHandler(this.buttonOrder_Click);
+            // 
+            // buttonRestaurants
+            // 
+            this.buttonRestaurants.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.buttonRestaurants.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRestaurants.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.buttonRestaurants.ForeColor = System.Drawing.Color.White;
+            this.buttonRestaurants.Location = new System.Drawing.Point(-1, 248);
+            this.buttonRestaurants.Name = "buttonRestaurants";
+            this.buttonRestaurants.Size = new System.Drawing.Size(263, 56);
+            this.buttonRestaurants.TabIndex = 1;
+            this.buttonRestaurants.Text = "Ресторани";
+            this.buttonRestaurants.UseVisualStyleBackColor = false;
+            // 
+            // buttonComment
+            // 
+            this.buttonComment.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.buttonComment.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonComment.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.buttonComment.ForeColor = System.Drawing.Color.White;
+            this.buttonComment.Location = new System.Drawing.Point(-1, 337);
+            this.buttonComment.Name = "buttonComment";
+            this.buttonComment.Size = new System.Drawing.Size(263, 56);
+            this.buttonComment.TabIndex = 2;
+            this.buttonComment.Text = "Залишити відгук";
+            this.buttonComment.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(128)))), ((int)(((byte)(191)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(683, 337);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(263, 56);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Увійти";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // FormPizzaService
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(944, 501);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.buttonComment);
+            this.Controls.Add(this.buttonRestaurants);
+            this.Controls.Add(this.buttonOrder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FormPizzaService";
+            this.Text = "Domino\'s Pizza";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOrder;
+        private System.Windows.Forms.Button buttonRestaurants;
+        private System.Windows.Forms.Button buttonComment;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ PizzaServiceEF/FormPizzaOrder.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace PizzaServiceEF
+{
+    partial class FormPizzaOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPagePizza = new System.Windows.Forms.TabPage();
+            this.tabPageDrinks = new System.Windows.Forms.TabPage();
+            this.tabPageDesserts = new System.Windows.Forms.TabPage();
+            this.tabPageSides = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPagePizza);
+            this.tabControl1.Controls.Add(this.tabPageDrinks);
+            this.tabControl1.Controls.Add(this.tabPageDesserts);
+            this.tabControl1.Controls.Add(this.tabPageSides);
+            this.tabControl1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tabControl1.Location = new System.Drawing.Point(12, 101);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(920, 388);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPagePizza
+            // 
+            this.tabPagePizza.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tabPagePizza.Location = new System.Drawing.Point(4, 25);
+            this.tabPagePizza.Name = "tabPagePizza";
+            this.tabPagePizza.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPagePizza.Size = new System.Drawing.Size(912, 359);
+            this.tabPagePizza.TabIndex = 0;
+            this.tabPagePizza.Text = "Піца";
+            this.tabPagePizza.UseVisualStyleBackColor = true;
+            // 
+            // tabPageDrinks
+            // 
+            this.tabPageDrinks.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tabPageDrinks.Location = new System.Drawing.Point(4, 25);
+            this.tabPageDrinks.Name = "tabPageDrinks";
+            this.tabPageDrinks.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageDrinks.Size = new System.Drawing.Size(912, 385);
+            this.tabPageDrinks.TabIndex = 1;
+            this.tabPageDrinks.Text = "Напої";
+            this.tabPageDrinks.UseVisualStyleBackColor = true;
+            // 
+            // tabPageDesserts
+            // 
+            this.tabPageDesserts.Location = new System.Drawing.Point(4, 25);
+            this.tabPageDesserts.Name = "tabPageDesserts";
+            this.tabPageDesserts.Size = new System.Drawing.Size(912, 385);
+            this.tabPageDesserts.TabIndex = 2;
+            this.tabPageDesserts.Text = "Десерти";
+            this.tabPageDesserts.UseVisualStyleBackColor = true;
+            // 
+            // tabPageSides
+            // 
+            this.tabPageSides.Location = new System.Drawing.Point(4, 25);
+            this.tabPageSides.Name = "tabPageSides";
+            this.tabPageSides.Size = new System.Drawing.Size(912, 385);
+            this.tabPageSides.TabIndex = 3;
+            this.tabPageSides.Text = "Сайди";
+            this.tabPageSides.UseVisualStyleBackColor = true;
+            // 
+            // FormPizzaOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(944, 501);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FormPizzaOrder";
+            this.Text = "Піца - замовити з доставкою | Domino\'s Pizza";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPagePizza;
+        private System.Windows.Forms.TabPage tabPageDrinks;
+        private System.Windows.Forms.TabPage tabPageDesserts;
+        private System.Windows.Forms.TabPage tabPageSides;
+    }
+}
